9f5247fd3d6891a12201327dac70c15be369e9fe
ColinAdo <colinadodev30@gmail.com>
2023-05-30 22:56:48 +0300

    Feat: Add Sign-Up authentication

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .import views
 
-app_name='name'
+app_name='core'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+from .models import Profile
 
 
 def home(request):
@@ -7,3 +11,35 @@ def home(request):
     context = {}
     return render(request, template, context)
 
+
+def signup(request):
+    template = 'core/signup.html'
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('core:signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('core:signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+
+                # Creating profile for a user
+                new_user = User.objects.get(username=username)
+                profile = Profile.objects.create(user=new_user)
+                profile.save()
+                messages.success(request, 'User created successfully, you can now login')
+                return redirect('core:signup')
+        else:
+            messages.info(request, 'Password mismatch')
+
+    context = {}
+    return render(request, template, context)
