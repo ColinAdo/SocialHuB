651d97886575794dcfe0fea862c3540052018db4
ColinAdo <colinadodev30@gmail.com>
2023-12-25 12:27:14 +0300

    Add correct grammer

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -11,26 +11,26 @@ def custom_timesince(value):
     if diff.days >= 365:
         years = diff.days // 365
         if years == 1:
-            return f"{years} year ago"
+            return "a year ago"
         return f"{years} years ago"
     elif diff.days >= 30:
         months = diff.days // 30
         if months == 1:
-            return f"{months} month ago"
+            return "a month ago"
         return f"{months} months ago"
     elif diff.days > 0:
         if diff.days == 1:
-            return f"{diff.days} day ago"
+            return "a day ago"
         return f"{diff.days} days ago"
     elif diff.seconds >= 3600:
         hours = diff.seconds // 3600
         if hours == 1:
-            return f"{hours} hour ago"
+            return "an hour ago"
         return f"{hours} hours ago"
     elif diff.seconds >= 60:
         minutes = diff.seconds // 60
         if minutes == 1:
-            return f"{minutes} minute ago"
+            return "a minute ago"
         return f"{minutes} minutes ago"
     else:
         return "Just now"
