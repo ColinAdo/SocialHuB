cfa0871433dd1302ee8abfc39fad507e0e3c4445
ColinAdo <colinadodev30@gmail.com>
2024-01-03 07:56:42 +0300

    Add test_views.py

---
+++ tests/core/test_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from core.models import Post, EmailVerification
+
+class TestHomeViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(username='user', password='password')
+        self.home_url = reverse('home')
+
+    def test_home_view_with_authenticated_user(self):
+        self.client.login(username=self.user.username, password=self.user.password)
+        EmailVerification.objects.filter(user=self.user, is_verified=True).exists()
+        response = self.client.get(self.home_url)
+
+        self.assertEquals(response.status_code, 302)
+        # self.assertTemplateUsed(response, 'core/index.html')
